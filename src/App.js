6eaf7e770d525e05233a4c47a54299f393764e2c
AndreiLeonov <andrei_leonov93@mail.ru>
2021-04-18 01:13:25 +0300

    map for categories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { Header, Categories } from './components';
+import React from "react";
+import { Header, Categories } from "./components";
 
 function App() {
+
+  const [] = React.useState()
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <Categories items={['мясные', 'вегетарианская', 'гриль', 'острые', 'закрытые']} />
+            <Categories
+              items={[
+                "Мясные",
+                "Вегетарианская",
+                "Гриль",
+                "Острые",
+                "Закрытые",
+              ]}
+              onClick={(el) => console.log(el)}
+            />
             <div className="sort">
               <div className="sort__label">
                 <svg
@@ -16,7 +28,8 @@ function App() {
                   height="6"
                   viewBox="0 0 10 6"
                   fill="none"
-                  xmlns="http://www.w3.org/2000/svg">
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
                     d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
                     fill="#2C2C2C"
@@ -62,7 +75,8 @@ function App() {
                     height="12"
                     viewBox="0 0 12 12"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white"
@@ -72,7 +86,7 @@ function App() {
                   <i>2</i>
                 </div>
               </div>
-            </div>{' '}
+            </div>{" "}
             <div className="pizza-block">
               <img
                 className="pizza-block__image"
@@ -99,7 +113,8 @@ function App() {
                     height="12"
                     viewBox="0 0 12 12"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white"
@@ -109,7 +124,7 @@ function App() {
                   <i>2</i>
                 </div>
               </div>
-            </div>{' '}
+            </div>{" "}
             <div className="pizza-block">
               <img
                 className="pizza-block__image"
@@ -136,7 +151,8 @@ function App() {
                     height="12"
                     viewBox="0 0 12 12"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white"
@@ -146,7 +162,7 @@ function App() {
                   <i>2</i>
                 </div>
               </div>
-            </div>{' '}
+            </div>{" "}
             <div className="pizza-block">
               <img
                 className="pizza-block__image"
@@ -173,7 +189,8 @@ function App() {
                     height="12"
                     viewBox="0 0 12 12"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white"
@@ -183,7 +200,7 @@ function App() {
                   <i>2</i>
                 </div>
               </div>
-            </div>{' '}
+            </div>{" "}
             <div className="pizza-block">
               <img
                 className="pizza-block__image"
@@ -210,7 +227,8 @@ function App() {
                     height="12"
                     viewBox="0 0 12 12"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white"
@@ -220,7 +238,7 @@ function App() {
                   <i>2</i>
                 </div>
               </div>
-            </div>{' '}
+            </div>{" "}
             <div className="pizza-block">
               <img
                 className="pizza-block__image"
@@ -247,7 +265,8 @@ function App() {
                     height="12"
                     viewBox="0 0 12 12"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white"
@@ -257,7 +276,7 @@ function App() {
                   <i>2</i>
                 </div>
               </div>
-            </div>{' '}
+            </div>{" "}
             <div className="pizza-block">
               <img
                 className="pizza-block__image"
@@ -284,7 +303,8 @@ function App() {
                     height="12"
                     viewBox="0 0 12 12"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white"
@@ -294,7 +314,7 @@ function App() {
                   <i>2</i>
                 </div>
               </div>
-            </div>{' '}
+            </div>{" "}
             <div className="pizza-block">
               <img
                 className="pizza-block__image"
@@ -321,7 +341,8 @@ function App() {
                     height="12"
                     viewBox="0 0 12 12"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white"
@@ -331,7 +352,7 @@ function App() {
                   <i>2</i>
                 </div>
               </div>
-            </div>{' '}
+            </div>{" "}
             <div className="pizza-block">
               <img
                 className="pizza-block__image"
@@ -358,7 +379,8 @@ function App() {
                     height="12"
                     viewBox="0 0 12 12"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white"
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-export const Categories = () => {
+export const Categories = ({items, onClick}) => {
   return (
     <div className="categories">
       <ul>
         <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {items.map((el, index) => <li onClick={() => onClick(el)} key={`${el}_${index}`}>{el}</li>)}
       </ul>
     </div>
   );
