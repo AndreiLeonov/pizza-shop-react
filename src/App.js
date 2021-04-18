ea15e8c174bf6da2b3b4f644a3fb18e8c06d5fd1
AndreiLeonov <andrei_leonov93@mail.ru>
2021-04-18 20:40:24 +0300

    select categories and check null or undefined

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Header, Categories } from "./components";
 
 function App() {
 
-  const [] = React.useState()
+  
 
   return (
     <div className="wrapper">
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-export const Categories = ({items, onClick}) => {
+export const Categories = ({ items, onClick }) => {
+  const [activeItem, setActiveItem] = React.useState(null);
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        {items.map((el, index) => <li onClick={() => onClick(el)} key={`${el}_${index}`}>{el}</li>)}
+        <li className={activeItem === null ? 'active' : ''} onClick={() => setActiveItem(null)}>
+          Все
+        </li>
+        {items && items.map((el, index) => (
+          <li
+            className={activeItem === index ? 'active' : ''}
+            onClick={() => setActiveItem(index)}
+            key={`${el}_${index}`}>
+            {el}
+          </li>
+        ))}
       </ul>
     </div>
   );
