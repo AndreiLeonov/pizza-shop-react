e91b3967e09dcf648d79133145980d4b3894ca0c
AndreiLeonov <andrei_leonov93@mail.ru>
2021-04-22 17:53:21 +0300

    added Pizza component and prepared json file + simulated server request

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
-import { Header } from "./components";
+import React from 'react';
+import { Header } from './components';
 import { Home, ShoppingCart } from './pages';
 import { Route } from 'react-router-dom';
 
 function App() {
+  const [pizzasData, setPizzasData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+    .then((res) => res.json())
+    .then((json) => setPizzasData(json.pizzas))
+  }, []);
+
+  console.log(pizzasData);
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path = "/" component = { Home } />
-        <Route exact path = "/cart" component = { ShoppingCart } />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/cart" component={ShoppingCart} />
       </div>
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import { Categories } from './Categories';
 import { Header } from './Header';
 import { Button } from './Button';
-import { SortPopup } from './SortPopup'
+import { SortPopup } from './SortPopup';
+import { Pizza } from './Pizza'
 
-export { Categories, Header, Button, SortPopup };
+export { Categories, Header, Button, SortPopup, Pizza };
