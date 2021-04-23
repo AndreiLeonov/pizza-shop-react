fdeedbca30ddf9d202c802bf5d54287b89613319
AndreiLeonov <andrei_leonov93@mail.ru>
2021-04-24 01:40:06 +0300

    added Redux & React-redux libs

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Categories = ({ items, onClick }) => {
+export const Categories = ({ items }) => {
   const [activeItem, setActiveItem] = React.useState(null);
 
   const onSelectItem = (index) => {
