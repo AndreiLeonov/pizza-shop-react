bb38b049629259f35083d26bece0b86b0fa1363a
AndreiLeonov <andrei_leonov93@mail.ru>
2021-05-04 19:10:00 +0300

    added JSON server

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const dispatch = useDispatch();
   
   React.useEffect(() => {
-    axios.get('http://localhost:3000/db.json').then((res) => dispatch(setPizzas(res.data.pizzas)));
+    axios.get('http://localhost:3001/pizzas').then((res) => dispatch(setPizzas(res.data)));
   }, []);
 
   return (
